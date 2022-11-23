686e15d2b0b73767a960107d067dcd217abffa01
Dorian Edwards <de.pharmd2018@gmail.com>
2022-11-22 19:23:33 -0500

    add patterns to home header on desktop view

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import Button from './Button'
 import Feature from './Feature'
 import Overview from './Overview'
 import { useMediaContext } from '../providers/MediaProvider'
 import PrimaryHeading from './Typography/PrimaryHeading'
+
+import rightArrow from '../public/assets/patterns/right-arrow.svg'
+import line from '../public/assets/patterns/line.svg'
+import whiteCircles from '../public/assets/patterns/white-circles.svg'
 
 import data from '../public/data.json'
 
@@ -10,15 +15,44 @@ export default function HomePage() {
   const { format } = useMediaContext()
   return (
     <>
-      <header className='header bg-home-mobile bg-no-repeat bg-cover bg-center min-h-[650px] tablet:bg-home-tablet desktop:bg-home-desktop pt-[115px] px-8 mb-[120px]'>
-        <div className='tablet:max-w-[573px] tablet:mx-auto'>
-          <PrimaryHeading>Scooter Sharing Made Simple</PrimaryHeading>
-          <p className='font-lexend text-[15px] leading-[25px] text-center text-white mb-[34px]'>
+      <header className='header bg-home-mobile bg-no-repeat bg-cover bg-center min-h-[650px] tablet:bg-home-tablet desktop:bg-home-desktop pt-[115px] px-8 mb-[120px] desktop:relative desktop:overflow-x-hidden'>
+        {format === 'desktop' && (
+          <Image
+            src={whiteCircles}
+            alt='two black circles with white outline and one solid white circle in a row'
+            width={234}
+            height={63}
+            className='absolute right-[-30px] top-[67.69%]'
+          />
+        )}
+        <div className='tablet:max-w-[573px] tablet:mx-auto desktop:max-w-[1110px]'>
+          <PrimaryHeading styling='desktop:mb-10'>
+            Scooter Sharing Made Simple
+          </PrimaryHeading>
+          <p className='font-lexend text-[15px] leading-[25px] text-center text-white mb-[34px] desktop:text-left desktop:max-w-[405px] desktop:ml-[95px] relative'>
             Scoot takes the hassle out of urban mobility. Our bikes are placed
             in convenient locations in each of our cities. Use our app to locate
             the nearest bike, unlock it with a tap, and you’re away!
+            {format === 'desktop' && (
+              <div className='arrow-pattern-wrapper'>
+                <Image
+                  src={rightArrow}
+                  alt='rightward pointing yellow arrow'
+                  width={452}
+                  height={151}
+                  className='absolute top-[54.46%] right-[-445px]'
+                />
+                <Image
+                  src={line}
+                  alt='straight, horizontal yellow line'
+                  width={203}
+                  height={15}
+                  className='absolute left-[-298px] top-[54.46%]'
+                />
+              </div>
+            )}
           </p>
-          <div className='button-wrapper flex justify-center'>
+          <div className='button-wrapper flex justify-center desktop:justify-start desktop:ml-[95px]'>
             <Button text='Get Scootin' />
           </div>
         </div>
--- components/Typography/PrimaryHeading.tsx
+++ components/Typography/PrimaryHeading.tsx
@@ -1,30 +1,16 @@
-// import { HeadingProps } from '../../Interfaces/Interfaces'
+export interface HeadingPrimaryProps {
+  children: string
+  styling?: string
+}
 
-// export interface Headings {
-//   [index: number]: JSX.Element
-// }
-
-// export default function Heading({ children, type }: HeadingProps) {
-//   const styles = [
-//     'font-space text-[56px] lead-[56px] tracking-[-2.5px]',
-//     'font-space text-[48px] lead-[48px] tracking-[-2.14px]',
-//     'font-space text-[40px] lead-[48px] tracking-[-1.79px]',
-//     'font-space text-[24px] lead-[28px] tracking-[-1.07px]',
-//   ]
-
-//   const headings: Headings = {
-//     0: <h1 className={`${styles[0]} text-dark-navy`}>{children}</h1>,
-//     1: <h2 className={`${styles[1]} text-dark-navy`}>{children}</h2>,
-//     2: <h3 className={`${styles[2]} text-dark-navy`}>{children}</h3>,
-//     3: <h4 className={`${styles[3]} text-dark-navy`}>{children}</h4>,
-//   }
-
-//   return <>{headings[type]}</>
-// }
-
-export default function HeadingPrimary({ children }: { children: string }) {
+export default function HeadingPrimary({
+  children,
+  styling,
+}: HeadingPrimaryProps) {
   return (
-    <h1 className='font-space text-center text-white text-[40px] lead-[48px] tracking-[-1.79px] tablet:text-[56px] tablet:lead-[56px] tablet:tracking-[-2.5px]'>
+    <h1
+      className={`font-space text-center text-white text-[40px] lead-[48px] tracking-[-1.79px] tablet:text-[56px] tablet:lead-[56px] tablet:tracking-[-2.5px] desktop:text-left desktop:max-w-[500px] ${styling}`}
+    >
       {children}
     </h1>
   )
