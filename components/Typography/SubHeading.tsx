7c06c4dec2be7ab0c86f05fd3cf6c7500bdeff0a
Dorian Edwards <de.pharmd2018@gmail.com>
2022-12-01 12:24:39 -0500

    change gitconfig to not ignore case

---
+++ components/Typography/SubHeading.tsx
@@ -0,0 +1,14 @@
+export interface SubHeadingProps {
+  children: string
+  styling?: string
+}
+
+export default function SubHeading({ children, styling }: SubHeadingProps) {
+  return (
+    <h2
+      className={`font-space text-[32px] text-dark-navy tracking-[-1.43px] text-center mb-8 ${styling}`}
+    >
+      {children}
+    </h2>
+  )
+}
