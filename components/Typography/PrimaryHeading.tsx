85ad1af3493e2b51aace23af05a4d51d4660a7da
Dorian Edwards <de.pharmd2018@gmail.com>
2022-12-02 08:56:22 -0500

    fix styling for line height on homepage

--- components/Typography/PrimaryHeading.tsx
+++ components/Typography/PrimaryHeading.tsx
@@ -9,7 +9,7 @@ export default function HeadingPrimary({
 }: HeadingPrimaryProps) {
   return (
     <h1
-      className={`font-space text-center text-white text-[40px] lead-[48px] tracking-[-1.79px] tablet:text-[56px] tablet:lead-[56px] tablet:tracking-[-2.5px] desktop:text-left desktop:max-w-[500px] ${styling}`}
+      className={`font-space text-center text-white text-[40px] leading-[48px] tracking-[-1.79px] tablet:text-[56px] tablet:leading-[56px] tablet:tracking-[-2.5px] desktop:text-left desktop:max-w-[500px] ${styling}`}
     >
       {children}
     </h1>
